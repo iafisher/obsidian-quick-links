5ed74dee0fd4db4ee1f840ab572f11cbdb118176
Ian Fisher <ian@iafisher.com>
2023-05-06 20:45:30 -0400

    Add blank entry for aesthetic purposes

--- main.ts
+++ main.ts
@@ -17,10 +17,16 @@ interface QuickLink {
 }
 
 const DEFAULT_QUICK_LINKS: QuickLinksSettings[] = [
+  // Wikipedia
   {
     prefix: "w",
     target:
       "https://www.wikipedia.org/search-redirect.php?family=wikipedia&language=en&go=Go&search=%s",
+  },
+  // Blank entry for the settings editor
+  {
+    prefix: "",
+    target: "",
   },
 ];
 
